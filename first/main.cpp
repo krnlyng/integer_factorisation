170f66295bdf845dd68c3b533536e1e5b10e6512
Franz-Josef Haider <f_haider@gmx.at>
2015-05-02 16:32:45 +0200

    implement usage and optional base argument

--- first/main.cpp
+++ first/main.cpp
@@ -84,11 +84,11 @@ tuple<number, number, bool> find_next_digits(const number &n, const int &current
     return make_tuple(1, n, false);
 }
 
-pair<number, number> factorize(const number &n)
+pair<number, number> factorize(const number &n, const number &base)
 {
     if(n != 0)
     {
-        tuple<number, number, bool> r = find_next_digits(n, 0, 0, 0, 10, 10, 1);
+        tuple<number, number, bool> r = find_next_digits(n, 0, 0, 0, base, base, 1);
 
         return make_pair(get<0>(r), get<1>(r));
     }
@@ -98,24 +98,48 @@ pair<number, number> factorize(const number &n)
     }
 }
 
+void usage(char *name)
+{
+    cout << "usage:" << endl;
+    cout << name << " [base] number" << endl;
+    cout << "\tbase\tis the base with which the algorithm should calculate, if not" << endl;
+    cout << "\t\tspecified base = 2 will be used." << endl;
+    cout << "\tnumber\tis the number which shall be factorized." << endl;
+}
+
 int main(int argc, char *argv[])
 {
     number n;
+    number base;
     pair<number, number> factors;
 
-    if(argc != 2)
+    if(argc != 2 && argc != 3)
     {
-        cerr << "please specify one (positive integer) argument to this program" << endl;
+        usage(argv[0]);
         return -1;
     }
 
+    if(argc == 2)
+    {
+        base = 2;
 #if USE_GMP
-    n = argv[1];
+        n = argv[1];
 #else
-    n = strtoll(argv[1], NULL, 10);
+        n = strtoll(argv[1], NULL, 10);
 #endif
+    }
+    else
+    {
+#if USE_GMP
+        base = argv[1];
+        n = argv[2];
+#else
+        base = strtoull(argv[1], NULL, 10);
+        n = strtoull(argv[2], NULL, 10);
+#endif
+    }
 
-    factors = factorize(n);
+    factors = factorize(n, base);
 
     if((factors.first == 1 || factors.second == 1) && !(factors.first == factors.second))
     {
